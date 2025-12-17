888a3a431dcb27e90614ef39b99beda6ed5e777e
Rranssu <aldwinlanceastillero0@gmail.com>
2025-12-18 03:19:38 +0800

    final commit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
