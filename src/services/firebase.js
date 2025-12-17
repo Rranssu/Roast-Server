661c104ac87cf4c9b2f0d100ebecacd139715d31
Rranssu <aldwinlanceastillero0@gmail.com>
2025-12-18 03:23:12 +0800

    hope this works

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,11 @@
 const admin = require('firebase-admin');
+
+// Use the JSON string from Render's env var
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://roast-531d6.firebaseio.com'
+  databaseURL: 'https://roast-531d6.firebaseio.com' // Replace with your Firebase project ID
 });
 
 const db = admin.firestore();
